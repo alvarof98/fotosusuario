c3891d93562bac843896e3e7ee6ff9a567f8d00c
alvarof98 <33150492+alvarof98@users.noreply.github.com>
2018-10-25 09:45:31 +0200

    Add files via upload

---
+++ practica2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=2.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Usuarios Imagenes</title>
+    </head>
+    <body>
+       <h2>Usuario nuevo: </h2>
+       <form>
+           <label>Nombre usuario: </label>
+           <input type="text" name="usuario"/>
+           <br/><br/>
+           <label>Foto:  </label>
+           <input type="file" name="archivo"/>
+           <br/><br/>
+           <input type="submit" value="Submit"/>
+       </form>
+       
+       <h2>Lista Usuarios: </h2>
+       <ul></ul>
+       
+    </body>
+</html>
